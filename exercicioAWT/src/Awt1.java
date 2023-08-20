3bb5e89778b926b033a5a24f2b7814ea44e3fdeb
Blue <101784753+blue2n@users.noreply.github.com>
2023-08-20 11:17:43 -0300

    Add files via upload

---
+++ exercicioAWT/src/Awt1.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+
+public class Awt1 {
+    public static void main(String [] args) {
+        Frame frame = new Frame("Exemplo AWT!");
+        Label label = new Label("Olá, mundo!");
+
+        frame.add(label);
+        frame.setSize(300, 200);
+        frame.setVisible(true);
+    }
+}
